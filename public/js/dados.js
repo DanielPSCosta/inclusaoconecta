0feae4b519c28d471bebb9c2a5b1e4c5e7549f7a
vapvuup <57815094+DanielPSCosta@users.noreply.github.com>
2026-06-23 18:33:53 -0300

    correção

--- public/js/dados.js
+++ public/js/dados.js
@@ -1,28 +1,5 @@
 
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    $('#table').bootstrapTable({
-        locale: 'pt-BR',
-
-        iconsPrefix: 'bi',
-        icons: {
-            paginationSwitchDown: 'bi-caret-down-fill',
-            paginationSwitchUp: 'bi-caret-up-fill',
-            refresh: 'bi-arrow-clockwise',
-            toggleOff: 'bi-list-ul',
-            toggleOn: 'bi-grid-3x3-gap',
-            columns: 'bi-layout-three-columns',
-            detailOpen: 'bi-plus',
-            detailClose: 'bi-dash',
-            fullscreen: 'bi-arrows-fullscreen',
-            export: 'bi-download'
-        }
-    });
-
-    carregarAtendimentos();
-
-});
 
 
 
