e22ce87163e933e0f8ec8e61532a5122ef010060
vapvuup <57815094+DanielPSCosta@users.noreply.github.com>
2026-06-23 19:09:58 -0300

    Alteração script

---
+++ public/PHP/css.php.php
@@ -0,0 +1,8 @@
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style_home.css">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Bootstrap Table -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/bootstrap-table.min.css">
