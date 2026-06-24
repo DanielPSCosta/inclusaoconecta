8349184170e66175ab441d963fde834a231d8c7b
vapvuup <57815094+DanielPSCosta@users.noreply.github.com>
2026-06-23 21:19:43 -0300

    correção

--- public/PHP/css.php
+++
@@ -1,8 +0,0 @@
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-  
-
-    <!-- Bootstrap Icons -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-
-    <!-- Bootstrap Table -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/bootstrap-table.min.css">
