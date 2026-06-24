56d127f468f3a5dd33451d51ce6ceba7e46ad5a4
vapvuup <57815094+DanielPSCosta@users.noreply.github.com>
2026-06-23 21:33:53 -0300

    correção

--- public/pages/cadastro.php
+++ public/pages/cadastro.php
@@ -94,18 +94,23 @@
                                             </div>
                                         </div>
                                         <div class="row">
+
                                             <div class="col-md-3 mb-3">
-                                                <label for="Duracao">Duração (minutos)</label>
-                                                <input type="number"
+                                                <label for="Duracao">Duração</label>
+                                                <input type="text"
                                                     class="form-control"
                                                     id="Duracao"
-                                                    min="1"
-                                                    max="480"
+                                                    placeholder="01:45"
+                                                    pattern="^\d{1,2}:\d{2}$"
                                                     required>
+                                                <small class="text-muted">Formato: Horas:Minutos (ex: 2:30)</small>
                                                 <div class="invalid-feedback">
-                                                    Favor preencher o campo Duração!
+                                                    Favor preencher no formato HH:MM
                                                 </div>
                                             </div>
+
+
+
                                             <div class="col-md-6 mb-3">
                                                 <label for="validationServer04">Categoria</label>
                                                 <select class="form-control  " id="categoria" required>
