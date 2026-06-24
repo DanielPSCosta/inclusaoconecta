e2d3795bf069d83d9acd1cb5d8b057815435b629
vapvuup <57815094+DanielPSCosta@users.noreply.github.com>
2026-06-23 21:18:43 -0300

    correção

--- public/pages/relatorio.php
+++ public/pages/relatorio.php
@@ -8,9 +8,14 @@
     <title>Doações | Inclusão conecta</title>
 
     <link rel="icon" type="image/png" href="../assets/img/logo.png">
-      <link rel="stylesheet" href="../css/style_home.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style_home.css">
 
-    <link rel="stylesheet" href="../PHP/css.php.php">
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+    <!-- Bootstrap Table -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/bootstrap-table.min.css">
 </head>
 
 <body>
@@ -121,8 +126,50 @@
 
 
 
-    <script src="../PHP/script.php"></script>
+
+
+
+    <!-- <a class="whatsapp-btn"
+        href=" https://wa.me//5511961194875?text=Tenho%20interesse%20em%20participar%20do%20projeto%20Olá%20Barbará"
+        aria-label="Chamar no WhatsApp"><i class="fa fa-whatsapp" aria-hidden="true"></i></a> -->
+
+    <script src="https://kit.fontawesome.com/775fd40529.js" crossorigin="anonymous"></script>
+
+    <!-- <script src="../js/bootstrap.bundle.min.js"></script> -->
     <script src="../js/dados.js"></script>
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <!-- Bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- XLSX -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js"></script>
+
+    <!-- jsPDF -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></script>
+
+    <!-- jsPDF AutoTable -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.8.2/jspdf.plugin.autotable.min.js"></script>
+
+    <!-- TableExport -->
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin/tableExport.min.js"></script>
+
+    <!-- Bootstrap Table -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/bootstrap-table.min.js"></script>
+
+    <!-- PT-BR -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/locale/bootstrap-table-pt-BR.min.js"></script>
+
+    <!-- Export Extension -->
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap-table@1.25.0/dist/extensions/export/bootstrap-table-export.min.js"></script>
+
+
+    <script src="https://unpkg.com/bootstrap-table@1.22.6/dist/extensions/mobile/bootstrap-table-mobile.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </body>
 
